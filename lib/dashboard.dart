096e59a6fc8bc50e75462cb917b9a09b892c5db8
zhongpeng.zhang <i_zhongpeng@163.com>
2022-06-15 00:24:21 +0800

    modify dashboard

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -20,11 +20,13 @@ class _DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Stack(
       children: [
         Positioned.fill(
           child: DecoratedBox(
-            decoration: BoxDecoration(gradient: gradient),
+            // decoration: BoxDecoration(gradient: gradient),
+            decoration: BoxDecoration(color: Color(0xFF26292E)),
           ),
         ),
         Scaffold(
@@ -35,57 +37,171 @@ class _DashboardState extends State<Dashboard> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Text(
-                              'Hi, George',
-                              style: TextStyle(
-                                  color: Color.fromRGBO(105, 108, 112, 1),
-                                  fontSize: 12),
-                            ),
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Text(
-                              'Dashboard',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w600),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const CircleAvatar(
-                        backgroundImage:
-                            AssetImage('images/dashboard/hero_icon.png'),
-                      )
-                    ],
-                  ),
-                  Card(
-                    elevation: 10,
-                    child: Stack(
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Row(
                       children: [
-                        Positioned.fill(
-                          child: Image.asset(
-                            'images/dashboard/card.jpg',
-                            fit: BoxFit.cover,
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'Hi, George',
+                                style: TextStyle(
+                                    color: Color.fromRGBO(105, 108, 112, 1),
+                                    fontSize: 14),
+                              ),
+                              SizedBox(
+                                height: 4,
+                              ),
+                              Text(
+                                'Dashboard',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ],
                           ),
+                        ),
+                        const CircleAvatar(
+                          backgroundImage:
+                              AssetImage('images/dashboard/hero_icon.png'),
                         )
                       ],
                     ),
                   ),
-                  Expanded(child: Container())
-                  // Positioned.fill(
-                  //   child: Image.asset(
-                  //     'images/dashboard/card.jpg',
-                  //     fit: BoxFit.cover,
-                  //   ),
-                  // )
+                  Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20.0)),
+                        child: Image.asset(
+                          'images/dashboard/card.jpg',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const Positioned(
+                        top: 30,
+                        left: 20,
+                        child: Text(
+                          'Monitor your ',
+                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        ),
+                      ),
+                      const Positioned(
+                        top: 55,
+                        left: 20,
+                        child: Text(
+                          'expenses',
+                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        ),
+                      ),
+                      Positioned(
+                          bottom: 20,
+                          left: 20,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(64, 26)),
+                              onPressed: () {},
+                              child: const Text('Get')))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Color.fromRGBO(50, 53, 58, 0.9),
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        ),
+                        // height: 200,
+                        width: size.width,
+                        child: Padding(
+                          child: Image.asset('images/img4.png'),
+                          padding: const EdgeInsets.all(20.0),
+                        ),
+                      ),
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Color.fromRGBO(50, 53, 58, 0.98),
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        ),
+                        // height: 200,
+                        width: size.width,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          child: Column(children: [
+                            Container(
+                              height: 5,
+                              width: 36,
+                              decoration: const BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8)),
+                                color: Color(0xFF6E7478),
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  'Balance',
+                                  style: TextStyle(color: Color(0xFF87888F)),
+                                ),
+                                ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      minimumSize: const Size(64, 20),
+                                    ),
+                                    onPressed: () {},
+                                    child: const Text(
+                                      'See more',
+                                      style: TextStyle(fontSize: 8),
+                                    ))
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'images/icon-dollar.png',
+                                  height: 24,
+                                  width: 24,
+                                ),
+                                const SizedBox(
+                                  width: 6,
+                                ),
+                                const Text(
+                                  '6,421.52',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w900,
+                                      fontSize: 24),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: const [
+                                Text(
+                                  '23 March',
+                                  style: TextStyle(color: Color(0xFF87888F)),
+                                ),
+                                Text(
+                                  '- \$813',
+                                  style: TextStyle(color: Color(0xFF87888F)),
+                                ),
+                              ],
+                            )
+                          ]),
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               ),
             ),
